41ae3586b723cf5deb37af6122d0c4b36ec41a62
entcheva <christina.entcheva@gmail.com>
2017-01-27 00:17:26 -0500

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-function setup() {
+var scl = 20
+var snake
+var food
 
+
+function setup() {
+  createCanvas(800, 600)
+  frameRate(10)
+  snake = new Snake()
+
+  food = createVector(random(width), random(height))
 }
 
-function draw() {
+function pickLocation() {
+  var cols = floor(width/scl)
+  var rows = floor(width/scl)
+  food = createVector(random(random(cols)), random(random(rows)))
+  food.mult(scl)
+}
 
+
+function draw() {
+  background(51)
+  snake.update()
+  snake.show()
+
+  // snake.eat()
+
+  fill(255, 204, 0)
+  rect(food.x, food.y, scl, scl)
+}
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.dir(0, -1)
+  } else if (keyCode === DOWN_ARROW) {
+    snake.dir(0, 1)
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.dir(1, 0)
+  } else if (keyCode === LEFT_ARROW) {
+    snake.dir(-1, 0)
+  }
 }
